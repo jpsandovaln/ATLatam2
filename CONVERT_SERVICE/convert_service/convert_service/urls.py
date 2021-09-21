031d421c3b6e008f59ce6d045ba58ef4e7329d61
Franco Cruz <36147203+Franco98404@users.noreply.github.com>
2021-09-21 00:27:40 -0300

    Update urls.py

--- CONVERT_SERVICE/convert_service/convert_service/urls.py
+++ CONVERT_SERVICE/convert_service/convert_service/urls.py
@@ -26,7 +26,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from convert_service.views import index, upload, download
+from convert_service.views import index
+from convert_service.views import upload
+from convert_service.views import download
 from django.contrib import admin
 from django.urls import path
 
