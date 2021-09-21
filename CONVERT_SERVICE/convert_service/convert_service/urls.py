4de1d7cabfefa992eabbf63188b43d76d5fd37fa
Franco <franco98404@gmail.com>
2021-09-20 23:50:47 -0300

    Django module intial commit

--- CONVERT_SERVICE/convert_service/convert_service/urls.py
+++ CONVERT_SERVICE/convert_service/convert_service/urls.py
@@ -1,3 +1,16 @@
+#
+# @video_converter.py Copyright (c) 2021 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information").  You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
+
 """convert_service URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +26,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from convert_service.views import index, upload, download
 from django.contrib import admin
 from django.urls import path
 
+#For upload a file
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index),
+    path('upload/', upload),
+    path('download/', download),
 ]
+
+#For devopment purpose
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
