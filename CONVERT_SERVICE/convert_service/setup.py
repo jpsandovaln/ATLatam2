e1761e2d399d43a7b0efc8ebdc6944d7a71c4b77
Matias Bonifacio <matiboni99@gmail.com>
2021-09-27 13:57:44 -0300

    Unnecessary folders delete

--- CONVERT_SERVICE/convert_service/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-setup(
-    name="modelspackage",
-    version="1.1",
-    description="Paquete de modelos de ffmpeg",
-    author="Matias",
-    packages=["models", "models.ffmpeg"]
-
-)
