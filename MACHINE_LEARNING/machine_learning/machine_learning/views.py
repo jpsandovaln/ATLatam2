582d479720129e363ba22c667a8e1e0025029a94
LCAS <l>
2021-09-28 16:41:38 -0500

    Machine Learning EndPoint V3

--- MACHINE_LEARNING/machine_learning/machine_learning/views.py
+++ MACHINE_LEARNING/machine_learning/machine_learning/views.py
@@ -1,18 +1,17 @@
 from django.core.files.storage import FileSystemStorage
 from django.views import View
 from django.http import HttpResponse
-from django.http import JsonResponse
 from pathlib import Path
-import json
 from models.prediccion import Prediccion
-from models.result import Result
 from zipfile import ZipFile
+from models.result import Result
+import json
+from django.http import JsonResponse
 
 
 class Recognizer(View):
 
     def post(self, request):
-        print(":*:*:*")
 
         uploaded_file = request.FILES['file']
         fs = FileSystemStorage()
@@ -24,16 +23,15 @@ class Recognizer(View):
         BASE_DIR = Path(__file__).resolve().parent.parent
         filepath1 = str(BASE_DIR) + "/media"
 
+        # unzip FIle
         with ZipFile('/'.join((filepath1, uploaded_file.name)), 'r') as zipObj:
             zipObj.extractall(filepath1)
 
-        #filepath = str(BASE_DIR) + "/media/"
+        # Call ML Prediction
+        result = Prediccion(filepath1+'/'+uploaded_file.name[:-4], word, percentage).predict(model)
 
-        #path = 'C:/Users/LCAS/PycharmProjects/PROG-102/Images'
-        result = Prediccion(filepath1+'/'+uploaded_file.name[:-4]).predict(model)
-
-        testing = {i: pred.as_dict() for i, pred in enumerate(result)}
-        #testing = [pred.as_dict() for pred in result]
+        #testing = {i: pred.as_dict() for i, pred in enumerate(result)}
+        testing = [pred.as_dict() for pred in result]
         print(testing)
-        return HttpResponse(testing, 'application/json')
-        #return JsonResponse(testing, safe=False)
+        return HttpResponse(json.dumps(testing), 'application/json')
+        # return JsonResponse(testing, safe=False)
