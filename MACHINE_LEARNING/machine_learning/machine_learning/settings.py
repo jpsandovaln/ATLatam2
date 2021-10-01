ca5171388238ef379e306ee097ccda45911f47aa
Franco <franco98404@gmail.com>
2021-09-30 21:14:31 -0300

    Copyright corrections

--- MACHINE_LEARNING/machine_learning/machine_learning/settings.py
+++ MACHINE_LEARNING/machine_learning/machine_learning/settings.py
@@ -1,5 +1,5 @@
 #
-# @resnet.py Copyright (c) 2021 Jalasoft.
+# @settings.py Copyright (c) 2021 Jalasoft.
 # Cl 26 Sur #48-41, Ayurá Center Edificio Union № 1376, Medellín, Colombia.
 # All rights reserved.
 #
