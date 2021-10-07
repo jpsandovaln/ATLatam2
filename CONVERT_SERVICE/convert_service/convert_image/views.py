3cd0c9a0e279f4fc8ad27343b99d904f44d5804e
christiantc <christiantambo15@gmail.com>
2021-10-07 10:57:24 -0400

    add exception for each convertImage params

--- CONVERT_SERVICE/convert_service/convert_image/views.py
+++ CONVERT_SERVICE/convert_service/convert_image/views.py
@@ -11,10 +11,7 @@
 #
 
 # Create your views here.
-from django.shortcuts import render
 from django.contrib.sites.shortcuts import get_current_site
-import os
-import uuid
 from django.core.files.storage import FileSystemStorage
 from django.views import View
 from django.http import HttpResponse
@@ -25,26 +22,33 @@ from .model.convert_image_params import ConvertImageParams
 
 
 class ImageConverter(View):
+
     def post(self, request):
         if request.method == 'POST':
-            # Upload the file
-            uploaded_file = request.FILES['file']
-            grayscale = request.POST['grayscale']
-            blur = request.POST['blur']
-            adaptive_sharpen = request.POST['adaptive_sharpen']
-            resize = request.POST['resize']
-            flip = request.POST['flip']
-            flop = request.POST['flop']
-            rotate = request.POST['rotate']
-            noise = request.POST['noise']
-            charcoal = request.POST['charcoal']
-            matrix = request.POST['matrix']
-            implode = request.POST['implode']
-            vignette = request.POST['vignette']
+            try:
+                # Upload the file
+                uploaded_file = request.FILES['file']
+            except:
+                result_error = {
+                    "status": "ERROR",
+                    "imageOutput": "NOT IMAGE"
+                }
+                return HttpResponse(json.dumps(result_error), 'application/json')
 
-            print(grayscale)
+            grayscale = self.except_request(request, 'grayscale')
+            blur = self.except_request(request, 'blur')
+            adaptive_sharpen = self.except_request(request, 'adaptive_sharpen')
+            resize = self.except_request(request, 'resize')
+            flip = self.except_request(request, 'flip')
+            flop = self.except_request(request, 'flop')
+            rotate = self.except_request(request, 'rotate')
+            noise = self.except_request(request, 'noise')
+            charcoal = self.except_request(request, 'charcoal')
+            matrix = self.except_request(request, 'matrix')
+            implode = self.except_request(request, 'implode')
+            vignette = self.except_request(request, 'vignette')
+
             fs = FileSystemStorage()
-
             # Save the file
             fs.save(uploaded_file.name, uploaded_file)
             file = uploaded_file.name
@@ -55,21 +59,35 @@ class ImageConverter(View):
             filepath = str(BASE_DIR) + "/media/" + filename
 
             # Create object image to convert image
-            image = ConvertImage()
-            param = ConvertImageParams(filepath, grayscale, blur, adaptive_sharpen, resize, flip, flop, rotate, noise,
+            param = ConvertImageParams(filepath, grayscale, blur, adaptive_sharpen, resize, flip, flop, rotate,
+                                       noise,
                                        charcoal,
                                        matrix, implode,
                                        vignette)
-            image.convert(param)
 
-            # get download path
-            final_path = image.get_final_path()
-            print(final_path)
-            result = {
-                "status": "OK",
-                "imageOutput": "http://" + str(get_current_site(request).domain) + "/" + final_path
-            }
+            try:
+                image = ConvertImage()
+                image.convert(param)
 
-            return HttpResponse(json.dumps(result), 'application/json')
+                # get download path
+                final_path = image.get_final_path()
+                result = {
+                    "status": "OK",
+                    "imageOutput": "http://" + str(get_current_site(request).domain) + "/" + final_path
+                }
+                return HttpResponse(json.dumps(result), 'application/json')
+            except:
+                result_error = {
+                    "status": "ERROR",
+                    "imageOutput": "NOT IMAGE"
+                }
+                return HttpResponse(json.dumps(result_error), 'application/json')
 
         return HttpResponse("Please, used method POST")
+
+    @staticmethod
+    def except_request(request, value):
+        try:
+            return request.POST[value]
+        except:
+            return False
