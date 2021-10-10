8c5969d74f9c42ed9ff1636e2eb3fbde940badfe
LCAS <l>
2021-10-09 21:20:21 -0500

    migrations and model changes

--- MACHINE_LEARNING/image_recognizer/image_recognizer_app/models.py
+++ MACHINE_LEARNING/image_recognizer/image_recognizer_app/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+
 class Assets(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    path = models.CharField(max_length=100)
+    path = models.CharField(max_length=50)
     checksum = models.CharField(max_length=100)
 
     class Meta:
         managed = True
 
     def __str__(self):
-        return 'record {} named {} with path {} and MD5 format {}'.format(self.id, self.name, self.path, self.checksum)
+        return '{} File: {}, stored in {} with MD5: {}'.format(self.id, self.name, self.path, self.checksum)
