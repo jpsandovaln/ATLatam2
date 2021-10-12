4915685c10e776e6ea9877652762eb5932fa17cf
LCAS <l>
2021-10-12 10:17:24 -0500

    imageRecognizer util enhancement

--- MACHINE_LEARNING/image_recognizer/image_recognizer_app/utils/imageRecognizer.py
+++ MACHINE_LEARNING/image_recognizer/image_recognizer_app/utils/imageRecognizer.py
@@ -1,8 +1,21 @@
-
+#
+# @imageRecognizer.py Copyright (c) 2021 Jalasoft.
+# Cl 26 Sur #48-41, Ayurá Center Edificio Union № 1376, Medellín, Colombia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
 import os
 from ..model.prediction import Prediction
 
+
 class ImageRecognizer:
+    """Calls a machine learning model to recognize objects in an image"""
+
     @staticmethod
     def recognize(path, request):
         ImageRecognizer.validate(path)
@@ -11,6 +24,7 @@ class ImageRecognizer:
         testing = [pred.as_dict() for pred in result]
         return testing
 
+    # check if folder path is valid
     @staticmethod
     def validate(path):
         if not os.path.isdir(path):
