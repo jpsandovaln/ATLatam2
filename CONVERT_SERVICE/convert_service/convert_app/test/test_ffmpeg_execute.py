98d819bfcddacf8571404770bd193c1005f649ba
Matias Bonifacio <matiboni99@gmail.com>
2021-10-14 01:52:30 -0300

    Update test_ffmpeg_execute.py

--- CONVERT_SERVICE/convert_service/convert_app/test/test_ffmpeg_execute.py
+++ CONVERT_SERVICE/convert_service/convert_app/test/test_ffmpeg_execute.py
@@ -1,3 +1,15 @@
+#
+# @test_ffmpeg_execute.py Copyright (c) 2021 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information").  You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
 from unittest import TestCase
 from ..model.ffmpeg.ffmpeg_execute import ffmpegexecute
 from pathlib import Path
